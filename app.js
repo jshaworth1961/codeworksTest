93cddfed3aaff5e91e623806b3647b4733d82a62
John S. Haworth <jshaworth@gmail.com>
2019-12-27 08:00:37 -0700

    initialized

---
+++ app.js
@@ -0,0 +1,19 @@
+let myName = "Jack";
+
+let jack =
+{
+    name:"Clarence"
+};
+
+console.log(myName);
+
+myName = "John";
+
+console.log(myName);
+
+console.log(jack);
+
+
+jack = "Jim";
+
+console.log(jack);
